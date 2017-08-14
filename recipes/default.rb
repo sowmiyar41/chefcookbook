029331d3513af451c990ae4c73b41f56a8b4b38b
root <root@ip-172-31-10-5.ec2.internal>
2017-08-14 11:08:18 +0000

    modified

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,11 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
+#INSTALL GIT
 package "git" do
  action :install
 end
 
+#u01 directory creation
 directory node['base']['dir'] do
  owner 'root'
  group 'root'
@@ -19,6 +20,7 @@ directory node['base']['dir'] do
   action :create
 end
 
+#/u01/git-repo directory creation
 directory node['git']['dir'] do
  owner 'root'
  group 'root'
@@ -26,10 +28,25 @@ directory node['git']['dir'] do
   action :create
 end
 
-
-git  node['git']['dir'] do
+#Check the chef environment and clone from git
+if node.chef_environment == "qa"
+  git  node['git']['dir'] do
   repository node['git']['url']
   reference "master"
   action :checkout
 end
-
+elsif node.chef_environment == "dev"
+ directory "/u01/dev" do
+ owner 'root'
+ group 'root'
+  mode '0755'
+  action :create
+end
+else
+directory "/u01/qa" do
+ owner 'root'
+ group 'root'
+  mode '0755'
+  action :create
+end
+end
